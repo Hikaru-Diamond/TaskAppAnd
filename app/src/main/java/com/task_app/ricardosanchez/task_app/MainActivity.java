d108967144fbd9ab249dab1768b99f404c0bc35d
RicardoSanchez <accel-diamond@hotmail.com>
2018-06-15 11:47:08 -0500

    Initial Commit

--- app/src/main/java/com/task_app/ricardosanchez/task_app/MainActivity.java
+++ app/src/main/java/com/task_app/ricardosanchez/task_app/MainActivity.java
@@ -1,13 +1,45 @@
 package com.task_app.ricardosanchez.task_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static String strTag = "RSV";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(strTag,"THIS IS A MESSAGE TO MY LOG - THE ACTIVITY WAS CREATED");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(strTag,"THE APPLICATION STARTED RUNNING. YAY.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(strTag,"THE APPLICATION STOPPED RUNNING. WELP.");
+    }
+
+    @Override
+    protected void onRestart (){
+        super.onRestart();
+        Log.d(strTag,"THE APPLICATION RESTARTED. WHAT?");
+    }
+
+    public void ShowNewForm (View vwView){
+        Log.d(strTag, "OPENING FORM FOR NEW TASK");
+
+        Intent ntnIntent = new Intent(this.getApplicationContext(), NewTaskActivity.class);
+
+        startActivity(ntnIntent);
     }
 }
